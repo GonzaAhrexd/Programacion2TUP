fadadf7548bf192342506572506d0e286c38540b
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2023-11-03 21:58:22 -0300

    Pilas

--- src/EstructuraLineal/Main.java
+++ src/EstructuraLineal/Main.java
@@ -5,20 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Cola colaSuper = new Cola();
-        colaSuper.enqueue(1);
-        colaSuper.enqueue(2);
-        colaSuper.enqueue(3);
-        colaSuper.enqueue(4);
-        colaSuper.enqueue(5);
-        colaSuper.enqueue(6);
-        System.out.println(colaSuper.getSize());
-        System.out.println("Cola del supermercado");
-        while(colaSuper.getSize() != 0){
-            System.out.println(colaSuper.getElement());
-            System.out.println("Presione una tecla luego de atender a un cliente");
-            String a = input.nextLine();
-            colaSuper.dequeue();
-        }
+       Stack platos = new Stack();
+       platos.push(1);
+        platos.push(2);
+        platos.push(3);
+        platos.push(4);
+
+        System.out.println(platos.getElement());
+        platos.peak();
+
+        System.out.println(platos.getElement());
+        platos.peak();
+
+        System.out.println(platos.getElement());
+        platos.peak();
     }
 }
--- src/EstructuraLineal/Stack.java
+++ src/EstructuraLineal/Stack.java
@@ -2,4 +2,49 @@ package EstructuraLineal;
 
 public class Stack {
 
+
+    private Node top = null;
+    private int size;
+    //Push - Peak - Pop
+
+
+    public Node getTop() {
+        return top;
+    }
+
+    public void setTop(Node top) {
+        this.top = top;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void push(int num) {
+        Node nodo = new Node(num);
+        nodo.setNext(top);
+        this.top = nodo;
+        this.size++;
+    }
+
+    public void peak() {
+        if (top != null) {
+            Node eliminar = this.top;
+            this.top = this.top.getNext();
+            eliminar.setNext(null);
+            this.size--;
+        }
+    }
+
+    public int getElement(){
+        if(top == null){
+            return 0;
+        }else{
+            return top.getData();
+        }
+    }
 }
